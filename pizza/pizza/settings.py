4947458d250a342fb9dc0ba977ac017976f58b76
Winston <88885652+Winstonjames2@users.noreply.github.com>
2022-07-06 14:32:30 +0630

    Update settings.py

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@tn)gjr=$8%8*t2huuhao%^6m5#43i9#snxe#62j5_j-a6#x8l'
-
+SECRET_KEY = "XXX
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
